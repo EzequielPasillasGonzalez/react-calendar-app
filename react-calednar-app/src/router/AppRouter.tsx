1cf18f5d7e7d6798098cffbd2d32df68ac5443df
Ezequiel <ingepasillasg@gmail.com>
2026-09-08 09:17:10 -0600

    Añadir estructura de autenticación y páginas de calendario; incluir estilos para la página de inicio de sesión

---
+++ react-calednar-app/src/router/AppRouter.tsx
@@ -0,0 +1,19 @@
+import { LoginPage } from "@/auth/";
+import { CalendarPage } from "@/calendar/pages/CalendarPage.tsx";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+export const AppRouter = () => {
+  const authStatus = "not-authenticated";
+
+  return (
+    <Routes>
+      {authStatus === "not-authenticated" ? (
+        <Route path="/auth/*" element={<LoginPage />} />
+      ) : (
+        <Route path="/*" element={<CalendarPage />} />
+      )}
+
+      <Route path="/*" element={<Navigate to={"/auth/login"} />} />
+    </Routes>
+  );
+};
