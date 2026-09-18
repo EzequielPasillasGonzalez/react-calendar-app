57c9482c02db443904eb86f521ef88cd6ed922cb
Ezequiel <ingepasillasg@gmail.com>
2026-09-18 09:47:52 -0600

    feat: inicializar la configuracion para hacerla PWA

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import "./styles.css";
 import { CalendarApp } from "@/CalendarApp.tsx";
 
+registerSW({ immediate: true });
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CalendarApp />
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,36 @@
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import { defineConfig } from "vite";
-
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true, // Permite probar el Service Worker en localhost
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "React Calendar App",
+        short_name: "CalendarApp",
+        description: "Mi aplicación de calendario PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/favicon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
